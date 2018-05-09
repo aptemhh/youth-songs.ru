862beab64bf74cbcc952f7edc48ec4d7b0ae6a52
aptemhh <aptemhh@mail.ru>
2018-05-09 22:25:48 +0600

    ну неконец выложил на гит

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,9 +1,12 @@
 var address = 'http://youth-songs.ru:8780/rest/';
 var find_url = address + 'find/';
+var party_url = address + 'party/nameParty';
 var min_size_font = 13;
 var cur_size_font = min_size_font;
 var max_size_font = 53;
 var delta_size_font = 5;
+var current_number_song = 0;
+var list_song_name;
 init();
 
 
@@ -13,10 +16,10 @@ function init()
 	showMenu();
     document.getElementById("textSong").style.fontSize = cur_size_font+'px';
     $("#finder").keyup(function(event) {
-    if (event.keyCode === 13) {
-        find();
-    }
-});
+		if (event.keyCode === 13) {
+			find();
+		}
+	});
 }
 
 function findRest(url)
@@ -25,9 +28,11 @@ function findRest(url)
 	{
 		var list = document.getElementById("list");
 		list.innerHTML = '';
+		list_song_name = [];
 		for(var k in data) 
 		{
-			createSlise( data[k]['number'], data[k]['description'], list);	 
+			list_song_name.push(data[k]['number']);
+			createSlise(data[k]['number'], data[k]['description'], list);	 
 		}
     });
 }
@@ -36,15 +41,9 @@ function createSlise(number, description, list)
 {
 	var	newlink = document.createElement('a');
 	newlink.setAttribute('class', 'list-group-item');
-	newlink.onclick = function() 
-	{
-		$.get(address+'number/'+number, function(data, status)
-		{
-			document.getElementById("textSong").innerHTML = transformTextToHTML(data['number'] +'. ' + data['text']);
-			hideMenu();
-		});
-	};
+	newlink.onclick = jumpToSong;
 	newlink.innerText = number + '. ' + description;
+	newlink.setAttribute('id', number);
 	list.appendChild(newlink)
 }
 
@@ -56,13 +55,16 @@ function find()
 function hideMenu() 
 {	
 	document.getElementById("menu").style.display='none';
+	document.getElementsByClassName("group")[0].style.display='none';
 	document.getElementById("menu2").style.display='block';
 }
 
 function showMenu() 
 {	
 	document.getElementById("menu").style.display='block';
+	document.getElementsByClassName("group")[0].style.display='block';
 	document.getElementById("menu2").style.display='none';
+	window.location.href = '#' + current_number_song;
 }
 function upSizeFont() 
 {	
@@ -84,5 +86,34 @@ String.prototype.replaceAll = function(search, replacement) {
 
 function transformTextToHTML(text)
 {
-    return text.replaceAll('(//[^]+//)', '<i>$1</i>').replaceAll('\n', '<br>').replaceAll('(Припев:*)', '<b>$1</b>');
+    return text.replaceAll('^([0-9]+)', '<b>$1</b>').replaceAll('(//[^]+//)', '<i>$1</i>').replaceAll('\n', '<br>').replaceAll('(Припев:*)', '<b>$1</b>');
 }
+
+function jumpToSong() 
+{
+	loadSong(this.getAttribute('id'));
+};
+
+function loadSong(number)
+{
+	$.get(address + 'number/' + number, 
+		function(data, status)
+		{
+			current_number_song = data['number'];
+			document.getElementById("textSong").innerHTML = transformTextToHTML(current_number_song +'. ' + data['text']);
+			hideMenu();
+		});
+}
+
+function nextSong()
+{
+	var id = list_song_name.indexOf(current_number_song) + 1;
+	loadSong(list_song_name[id == list_song_name.length ? id - 1: id]);
+}
+function downSong()
+{
+	var id = list_song_name.indexOf(current_number_song) - 1;
+	loadSong(list_song_name[id == -1 ? 0 : id]);
+}
+
+	
